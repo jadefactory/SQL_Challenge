523dd6c2bd2a8a84b7eab7f546cb9b497c77772f
jadefactory <seychelle86@gmail.com>
2022-02-04 10:38:44 +0900

    sql challenge

--- MySQL/subject/revenue.sql
+++ MySQL/subject/revenue.sql
@@ -1,5 +1,8 @@
--- Q1. 2020년 7월의 총 Revenue를 구해주세요. 
+-- Q1. 2020년 7월의 총 Revenue를 구해주세요.
+
 select sum(price) as revenue
 from tbl_purchase
 where purchased_at >= '2020-07-01' 
   and purchased_at < '2020-08-01';
+
+-- Revenue는 매출 총액을 의미함
