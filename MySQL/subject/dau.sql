f302316a2552692433f90cde6f461eb995c5080c
jadefactory <seychelle86@gmail.com>
2022-02-08 15:44:55 +0900

    sql challenge

--- MySQL/subject/dau.sql
+++ MySQL/subject/dau.sql
@@ -12,3 +12,6 @@ from (
 -- date_format에서 '%Y-%m-%d T'를 통해 시간대를 확인했을 때
 -- 실제 시간보다 9시간이 더해진 상태로 표시되어있기 때문에 
 -- 각각의 시간 데이터에서 9시간을 빼주지 않으면 8월 1일까지 집계됨
+-- interval n hour 메소드를 사용하여 시간차를 해결
+
+-- Active User 수의 추세는 dau_visualization.ipynb파일 참고
