dec922067f862e675e1122ea7df0e65dcd2cd98d
jadefactory <seychelle86@gmail.com>
2022-02-24 10:40:48 +0900

    sql challenge

--- MySQL/hackerrank/the_blunder.sql
+++ MySQL/hackerrank/the_blunder.sql
@@ -1,7 +1,9 @@
--- Question) 
+-- [ Question ]
 -- Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's key was broken until after completing the calculation. 
 -- She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
 -- Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
 
 select ceil(avg(Salary) - avg(replace(Salary,'0','')))
 from EMPLOYEES
+
+-- https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true
