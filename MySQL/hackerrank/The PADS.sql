a901f23cdbd30bad1d66c50bb31797c0f5c53bdf
jadefactory <seychelle86@gmail.com>
2022-03-06 21:02:32 +0900

    sql challenge

--- MySQL/hackerrank/The PADS.sql
+++ MySQL/hackerrank/The PADS.sql
@@ -1,8 +1,16 @@
-select concat(Name, '(', substr(Occupation, 1,1), ')')
+-- [ Question ]
+-- 1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+-- 2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+-- "There are a total of [occupation_count] [occupation]s."
+-- where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+
+select concat(Name, '(', substr(Occupation, 1, 1), ')')
 from OCCUPATIONS
 order by Name;
 
-select concat('There are a total of', count(Occupation), ' ', lower(Occupation), 's.'))
+select concat('There are a total of ', count(Occupation), ' ', lower(Occupation), 's.')
 from OCCUPATIONS
 group by Occupation
 order by count(occupation), occupation
+
+-- https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
