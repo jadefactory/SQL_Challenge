d45fa1000bf82822750cdc89e88f6d42fb2968dc
장환영 <53116412+jadefactory@users.noreply.github.com>
2022-03-18 00:08:51 +0900

    sql challenge

--- MySQL/hackerrank/occupations.sql
+++ MySQL/hackerrank/occupations.sql
@@ -1,5 +1,6 @@
 -- [ Question ]
--- Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+-- Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. 
+-- The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
 -- Note: Print NULL when there are no more names corresponding to an occupation.
 
 select min(case when A.OCCUPATION = 'Doctor' then name else null end) as Doctor,
